a5c593c07e5eb959b3abad37b6c86c99761a5fa2
unknown <yashgandhijee2018@gmail.com>
2022-11-04 23:17:10 +0530

    db Setup online

--- src/main/java/com/team4/luma/LumaApplication.java
+++ src/main/java/com/team4/luma/LumaApplication.java
@@ -1,9 +1,17 @@
 package com.team4.luma;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication()
+@ComponentScan(basePackages = "com.team4.luma")
+@EnableJpaRepositories(basePackages = "com.team4.luma")
+@Slf4j
+@Configuration
 public class LumaApplication {
 
 	public static void main(String[] args) {
