f450084cc8d8cbbe7dbf0b475f71643e986bf1f1
Vinod M <vinod820>
2022-11-04 17:49:59 +0530

    endpoints list added:

--- src/main/java/com/team4/luma/controller/LumaController.java
+++ src/main/java/com/team4/luma/controller/LumaController.java
@@ -1,20 +1,27 @@
 package com.team4.luma.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.team4.luma.dto.EmployeeDto;
 import com.team4.luma.dto.LoginDto;
+import com.team4.luma.entity.ItemEntity;
 import com.team4.luma.service.EmployeeService;
 
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping("/api")
 public class LumaController {
 	
 	@Autowired 
@@ -55,13 +62,28 @@ public class LumaController {
 		return new ResponseEntity<String>("Loan sanctioned", null, 200);
 	}
 	
-	@GetMapping("/loans/<employeeId>")
-	public ResponseEntity<String> getAllLoans(@RequestParam String employeeId){
+	@GetMapping("/loans/{employeeId}")
+	public ResponseEntity<String> getAllLoans(@PathParam(value = "employeeId") String employeeId){
 		return new ResponseEntity<String>("Loan sanctioned", null, 200);
 	}
 	
-	@GetMapping("/items/<employeeId>")
-	public ResponseEntity<String> getAllItemsPurchased(@RequestParam String employeeId){
+	@GetMapping("/items/{employeeId}")
+	public ResponseEntity<String> getAllItemsPurchased(@PathParam(value = "employeeId") String employeeId){
 		return new ResponseEntity<String>("Loan sanctioned", null, 200);
+	}
+	
+	@GetMapping("/item_category")
+	public List<String> getAllItemCategories(){
+		return new ArrayList<>();
+	}
+	
+	@GetMapping("/item_make/{category}")
+	public List<String> getAllItemMakeType(@PathParam(value = "category") String category){
+		return new ArrayList<>();
+	}
+	
+	@GetMapping("/all_items/{category}/{make}")
+	public List<ItemEntity> getAllItemList(@PathParam(value = "category") String category, @PathParam("make") String make){
+		return new ArrayList<>();
 	}
 }
