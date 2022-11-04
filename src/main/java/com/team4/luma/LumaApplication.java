27fa6837164ee14a39415fb7ed983d464533a0bc
unknown <yashgandhijee2018@gmail.com>
2022-11-04 10:04:42 +0530

    initialized the project

---
+++ src/main/java/com/team4/luma/LumaApplication.java
@@ -0,0 +1,13 @@
+package com.team4.luma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumaApplication.class, args);
+	}
+
+}
