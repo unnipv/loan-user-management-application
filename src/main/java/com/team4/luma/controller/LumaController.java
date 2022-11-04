e20369176c7d569ac76845b6c0f4fbdaadc9d00e
Vinod M <vinod820>
2022-11-04 12:19:35 +0530

    package updated

---
+++ src/main/java/com/team4/luma/controller/LumaController.java
@@ -0,0 +1,8 @@
+package com.team4.luma.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LumaController {
+
+}
---
+++ src/main/java/com/team4/luma/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.team4.luma.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.team4.luma.model.EmployeeMaster;
+
+public interface EmployeeRepository extends JpaRepository<EmployeeMaster, String> {
+
+}
