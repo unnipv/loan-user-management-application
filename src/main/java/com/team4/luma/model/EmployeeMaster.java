1ae38e098ea96c36e63f1ccdafd2e2aa03ffc7d9
unknown <yashgandhijee2018@gmail.com>
2022-11-04 13:32:38 +0530

    updated EmployeeMaster

--- src/main/java/com/team4/luma/model/EmployeeMaster.java
+++ src/main/java/com/team4/luma/model/EmployeeMaster.java
@@ -3,10 +3,17 @@ package com.team4.luma.model;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class EmployeeMaster {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String employeeId;
+	
 	private String employeeName;
 	private String designation;
 	private String department;
